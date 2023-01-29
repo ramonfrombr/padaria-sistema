7e7d398969482f72d645ca52fedf2039e8ae0c41
ramonfrombr <ramonfrombr@gmail.com>
2023-01-29 11:35:18 -0300

    firebase emulator support. styles on navbar

--- src/firebase.ts
+++ src/firebase.ts
@@ -38,4 +38,17 @@ if (Number(process.env.REACT_APP_DEVELOPMENT_MODE)) {
   db = getFirestore(app);
 }
 
+const firebaseConfig = {
+  apiKey: process.env.APP_ENV_FIREBASE_API_KEY,
+  authDomain: process.env.APP_ENV_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.APP_ENV_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.APP_ENV_FIREBASE_STORE_BUCKET,
+  messagingSenderId: process.env.APP_ENV_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.APP_ENV_FIREBASE_APP_ID
+};
+
+app = initializeApp(firebaseConfig);
+auth = getAuth(app);
+db = getFirestore(app);
+
 export { app, auth, db };
