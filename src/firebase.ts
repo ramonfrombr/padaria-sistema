e4dd89af358409977ac5bbef52a0658e13c07d95
ramonfrombr <ramonfrombr@gmail.com>
2023-01-30 02:25:58 -0300

    firebase emulator support. styles on navbar

--- src/firebase.ts
+++ src/firebase.ts
@@ -7,7 +7,6 @@ let app: FirebaseApp;
 let db: Firestore;
 let auth: Auth;
 
-/*
 if (Number(process.env.REACT_APP_DEVELOPMENT_MODE)) {
 
   app = initializeApp({
@@ -26,30 +25,18 @@ if (Number(process.env.REACT_APP_DEVELOPMENT_MODE)) {
 } else {
 
   const firebaseConfig = {
-    apiKey: process.env.APP_ENV_FIREBASE_API_KEY,
-    authDomain: process.env.APP_ENV_FIREBASE_AUTH_DOMAIN,
-    projectId: process.env.APP_ENV_FIREBASE_PROJECT_ID,
-    storageBucket: process.env.APP_ENV_FIREBASE_STORE_BUCKET,
-    messagingSenderId: process.env.APP_ENV_FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.APP_ENV_FIREBASE_APP_ID
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID
   };
 
   app = initializeApp(firebaseConfig);
   auth = getAuth(app);
   db = getFirestore(app);
-}*/
+}
 
-const firebaseConfig = {
-  apiKey: process.env.APP_ENV_FIREBASE_API_KEY,
-  authDomain: process.env.APP_ENV_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.APP_ENV_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.APP_ENV_FIREBASE_STORE_BUCKET,
-  messagingSenderId: process.env.APP_ENV_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.APP_ENV_FIREBASE_APP_ID
-};
-
-app = initializeApp(firebaseConfig);
-auth = getAuth(app);
-db = getFirestore(app);
 
 export { app, auth, db };
